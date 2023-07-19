9303e4bc4cef080d0669d59fd91eb10ec6f328b1
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-19 22:50:47 +0300

    utils: fix the className dot error

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,6 @@ export const refreshTurnstile = (props: Props = {}) => {
   if (typeof window === 'undefined') return
   if ('turnstile' in window) {
     // @ts-ignore
-    window.turnstile.render(props.className || defaultClassName)
+    window.turnstile.render('.' + (props.className || defaultClassName))
   }
 }
